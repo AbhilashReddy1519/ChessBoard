2d1b02b2e3c2d72a0d55e06d857f0c15edbaae36
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-01-17 11:21:52 +0530

    updated code and created chessboard and loaded images in chess board

--- chess/chessMain.py
+++ chess/chessMain.py
@@ -8,7 +8,8 @@ from chess import chessEngine
 
 WIDTH = HEIGHT = 512 #400 is another option
 DIMENSION = 8 #dimensions of a chess board are 8x8
-SQ_SIZE = 15 #for animation later on
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 #for animation later on
 IMAGES = {}
 
 '''
@@ -31,6 +32,70 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = chessEngine.GameState()
+    # print(gs.board)
+    loadImages() #only do this once before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+'''
+Responsible for all the graphics within a current game state.
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw squares on the board
+    #add in piece highlighting or move suggestions(later)
+    drawPieces(screen, gs.board) #draw pieces on top of these squares
+
+'''
+Draw the squares on the board.The top left square is always light.
+'''
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
-'''end of the day'''
+'''
+Draw the pieces on the board using the current GameState.board
+'''
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not empty square
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
