2bc61fb9673e96f6dfaddf4e741c838d23ba445d
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-01-18 19:24:16 +0530

    Add move validation, undo functionality, and move generation
    This commit introduces move validation and undo functionality to the chess engine. Additionally, functions to generate possible moves for pawns and rooks were added, with validation for turn-based moves. The move class now includes an equality check for comparing moves.

--- chess/chessMain.py
+++ chess/chessMain.py
@@ -32,6 +32,9 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = chessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False #flag variable for when a move is made
+
     # print(gs.board)
     loadImages() #only do this once before the while loop
     running = True
@@ -42,6 +45,7 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            #mouse handler
             elif e.type == p.MOUSEBUTTONDOWN:
                 location = p.mouse.get_pos() #(x, y) location of mouse
                 col = location[0]//SQ_SIZE
@@ -55,10 +59,21 @@ def main():
                 if len(playerClicks) == 2: #after 2nd click
                     move = chessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = () #reset user clicks
                     playerClicks = []
+            #key handlers
+            elif e.type == p.KEYDOWN:
+                    if e.key == p.K_z: #undo when 'z' is pressed
+                        gs.undoMove()
+                        moveMade = True
 
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
 
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
